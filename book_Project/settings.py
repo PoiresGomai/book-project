ddb264a8a6560c3d8264eb08979980e08c1da514
PoiresGomai <128164651+PoiresGomai@users.noreply.github.com>
2025-05-27 17:20:42 +0800

    Update settings.py

--- book_Project/settings.py
+++ book_Project/settings.py
@@ -12,7 +12,7 @@ DEBUG = True
 # 设置允许哪些主机访问我们的django后台站点，
 # 如果项目上线部署到远程服务器，那就必须设置allow_host为本地的ipv4地址
 # (设置为"*"也可以，但是不安全)，否则本地是无法访问远程的django站点
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # 应用程序定义
